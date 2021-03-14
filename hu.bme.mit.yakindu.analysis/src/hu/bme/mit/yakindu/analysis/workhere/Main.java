eb518ff346d03e6e362846b9adb8599a6491e7db
Katreiner Hanga <hanga.katreiner@gmail.com>
2021-03-14 23:34:11 +0100

    4.7 Change statechart and change Main.java

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -1,38 +1,63 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
-import org.yakindu.sct.model.sgraph.State;
+import org.yakindu.base.types.Event;
+import org.yakindu.base.types.Property;
+import org.yakindu.sct.model.sgraph.Scope;
 import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.stext.stext.EventDefinition;
+import org.yakindu.sct.model.stext.stext.VariableDefinition;
 
 import hu.bme.mit.model2gml.Model2GML;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
+import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
 
 public class Main {
 	@Test
-	public void test() {
+	public void test() throws IOException {
 		main(new String[0]);
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 		ModelManager manager = new ModelManager();
 		Model2GML model2gml = new Model2GML();
-		
+
 		// Loading model
 		EObject root = manager.loadModel("model_input/example.sct");
-		
+
 		// Reading model
 		Statechart s = (Statechart) root;
 		TreeIterator<EObject> iterator = s.eAllContents();
+		//System.out.println("public static void print(IExampleStatemachine s){");
 		while (iterator.hasNext()) {
 			EObject content = iterator.next();
-			if(content instanceof State) {
-				State state = (State) content;
-				System.out.println(state.getName());
+			if(content instanceof Scope) {
+				Scope scope = (Scope) content;
+				for(Event event: scope.getEvents()) {
+					System.out.println("BufferedReader br = new BufferedReader(new InputStreamReader(System.in));");
+					System.out.println("String eventName = br.readLine();");
+					System.out.println("System.out.println(eventName);");
+					for(Property variable: scope.getVariables()) {
+						System.out.println("System.out.println(" + variable.getName().toUpperCase().charAt(0)
+								+ " = s.getSCInterface().get" + variable.getName().toUpperCase().charAt(0) + variable.getName().substring(1) + "());");
+					}
+					System.out.println("switch (eventName) {");
+					System.out.println("  case " + event.getName() + ":");
+					System.out.println("    sm.raise" + event.getName().toUpperCase().charAt(0) + event.getName().substring(1) + "();");
+					System.out.println("    break;");
+				}
+
 			}
 		}
-		
 		// Transforming the model into a graph representation
 		String content = model2gml.transform(root);
 		// and saving it
